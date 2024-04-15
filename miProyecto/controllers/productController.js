56e2caa2cb595933f52982dea0de6b12c42923ea
CatalinaRod <cata@MacBook-Air-de-Cata.local>
2024-04-15 10:54:02 -0300

    controladores

--- miProyecto/controllers/productController.js
+++ miProyecto/controllers/productController.js
@@ -1,1 +1,40 @@
-// hacer un for para que se muestren todas las zapatillas
+const db = require('../db/index');
+
+const productController = {
+
+    detalleProducto: function (req, res) {
+        let idEnviado = req.params.id;
+        let detalleProducto = [];
+        let comentariosProducto = [];
+        for (let i = 0; i < data.productos.length; i++) {
+            if (idEnviado == data.productos[i].id) {
+                detalleProducto.push(data.productos[i]);
+                comentariosProducto = data.productos[i].comentarios; 
+                break;
+            }
+        }
+        return res.render('product', {
+            index: detalleProducto,
+            comentarios: comentariosProducto 
+        })
+    },
+
+    index: function(req, res){
+        return res.render("product", {'data': data
+        })
+    },
+    resultadosDeBusqueda: function(req, res){
+        return res.render("search-results", {'data': data
+        })
+    },
+    add: function(req,res) {
+        
+        return res.render("product-add", {
+            productos: data.productos
+        })
+    }
+}
+
+module.exports = productController
+
+
