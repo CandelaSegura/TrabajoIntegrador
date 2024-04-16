df7c74759d7a2f6c047d906980de41b42568a15b
CandelaSegura <mcsegura@udesa.edu.ar>
2024-04-16 12:32:08 -0300

    conexion de formularios

--- miProyecto/controllers/userController.js
+++ miProyecto/controllers/userController.js
@@ -3,19 +3,19 @@ const db = require('../db/index');
 const userController = {
     
     login: function(req,res) {
-        return res.render ('login', {'datos':db})
+        return res.render ('login', {'datos':db});
     },
 
     register: function(req,res) {
-        return res.render ('register', {'datos':db})
+        return res.render ('register', {'datos':db});
     },
 
     profile: function(req,res) {
-        return res.render ('profile',{'datos':db})
+        return res.render ('profile',{'datos':db});
     },
 
     profileEdit: function(req,res) {
-        return res.render ('profile-edit', {'datos':db})
+        return res.render ('profile-edit', {'datos':db});
     }
 }
 
