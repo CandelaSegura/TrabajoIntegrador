ce2adf1215338b22d7f3ccedfaf5d1c1b4b81878
CatalinaRod <cata@MacBook-Air-de-Cata.local>
2024-04-16 12:00:21 -0300

    areglo+

--- miProyecto/controllers/indexController.js
+++ miProyecto/controllers/indexController.js
@@ -2,7 +2,7 @@ const db = require('../db/index');
 
 const indexController = {
     index: function(req, res){
-        return res.render("index", {'data': data })
+        return res.render("index", {'data': db })
     }
 }
 
