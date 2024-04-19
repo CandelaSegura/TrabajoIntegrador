c13f08d02e2df0855e516a1562f3a077877a2ac3
CatalinaRod <cata@MacBook-Air-de-Cata.local>
2024-04-19 10:38:12 -0300

    visual

--- miProyecto/db/index.js
+++ miProyecto/db/index.js
@@ -9,8 +9,8 @@ let db = {
     },
     productos: [
         {
-            imagen:'/images/products/Gazelle.jpg',
-            nombre: 'ZAPATILLAS GAZELLE', //https://www.adidas.com.ar/zapatillas-gazelle/IF3817.html
+            imagen:'/images/products/1.jpg',
+            nombre: 'Gazelle', //https://www.adidas.com.ar/zapatillas-gazelle/IF3817.html
             descripcion: 'Zapatillas adidas con un look retro y una energia moderno',
             comentarios: [
                 {
@@ -27,8 +27,8 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/Samba.jpg',
-            nombre: 'ZAPATILLAS SAMBA OG', //https://www.adidas.com.ar/zapatillas-samba-og/ID0478.html
+            imagen:'/images/products/2.jpg',
+            nombre: 'Samba OG', //https://www.adidas.com.ar/zapatillas-samba-og/ID0478.html
             descripcion: 'Zapatillas atemporales en colores llamativos y materiales de alta costura',
             comentarios: [
                 {
@@ -45,7 +45,7 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/AirJordan.jpg',
+            imagen:'/images/products/3.jpg',
             nombre: 'Air Jordan 1 Low', //https://www.nike.com.ar/air-jordan-1-low-dc0774-200/p
             descripcion: 'El Air Jordan 1 Low te ofrece una parte de la historia y herencia Jordan con una comodidad que dura todo el día',
             comentarios: [
@@ -63,8 +63,8 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/AirJordanRetro.jpg',
-            nombre: 'Air Jordan 1 Retro High OG', //https://www.nike.com.ar/air-jordan-1-retro-high-og-fd2596-600/p?snrai_campaign=SC4AKZsAHFDu&snrai_id=279cc1b2-e257-4d50-8021-c7a70148eafe
+            imagen:'/images/products/4.jpg',
+            nombre: 'Air Jordan 1 Retro', //https://www.nike.com.ar/air-jordan-1-retro-high-og-fd2596-600/p?snrai_campaign=SC4AKZsAHFDu&snrai_id=279cc1b2-e257-4d50-8021-c7a70148eafe
             descripcion: 'Esta iteración del AJ1 reinventa el primer modelo exclusivo de Mike con una nueva combinación de colores',
             comentarios: [
                 {
@@ -81,8 +81,8 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/Palermo.jpg',
-            nombre: 'Zapatillas Palermo Lth unisex', //https://ar.puma.com/zapatillas-palermo-lth-unisex-396464-03.html?color=13364
+            imagen:'/images/products/5.jpg',
+            nombre: 'Palermo Lth unisex', //https://ar.puma.com/zapatillas-palermo-lth-unisex-396464-03.html?color=13364
             descripcion: 'Exterior de piel con superposiciones de ante y formstrip, Ojetera, lengüeta y etiqueta PUMA de material sintético',
             comentarios: [
                 {
@@ -99,8 +99,8 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/Suede.jpg',
-            nombre: 'Zapatillas Suede Classic XXI ADP', //https://ar.puma.com/zapatillas-suede-classic-xxi-adp-388981-90.html?color=12084
+            imagen:'/images/products/6.jpg',
+            nombre: 'Suede Classic XXI', //https:// ar.puma.com/zapatillas-suede-classic-xxi-adp-388981-90.html?color=12084
             descripcion: 'Diseño clásico de caña baja, Empeine de ante completo con forro sintético, Plantilla cómoda, Entresuela de goma, Suela exterior de goma y Cierre con cordones',
             comentarios: [
                 {
@@ -117,7 +117,7 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/ChuckTaylor.jpg',
+            imagen:'/images/products/7.jpg',
             nombre: 'Chuck Taylor All Star', //https://converse.com.ar/product/756991C
             descripcion: 'Un icono para el dia a dia. Es difícil negar que los niños se ven realmente lindos con las zapatillas Chuck Taylor All Star',
             comentarios: [
@@ -135,7 +135,7 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/Chuck70.jpg',
+            imagen:'/images/products/8.jpg',
             nombre: 'Chuck 70', //https://converse.com.ar/product/A05114C
             descripcion: 'Las Chuck 70 combinan los mejores detalles de las Chuck de los 70 con una confección artesanal impecable y materiales premium',
             comentarios: [
@@ -153,7 +153,7 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/500.jpeg',
+            imagen:'/images/products/9.jpg',
             nombre: '500', //https://www.newbalance.com.ar/mujer-zapatillas-N10190280.html
             descripcion: 'La zapatilla para mujer 500 te proporciona el confort que buscas en tu día a día sin renunciar al estilo',
             comentarios: [
@@ -171,7 +171,7 @@ let db = {
             ]
         },
         {
-            imagen:'/images/products/515v3.jpg',
+            imagen:'/images/products/10.jpg',
             nombre: '515 v3', //https://www.newbalance.com.ar/mujer-zapatillas-N10190272060799.html
             descripcion: 'El estilo informal se suma a la comodidad de nuestras zapatillas para mujer 515v3',
             comentarios: [
