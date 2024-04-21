02ffca57977a5624eb8c48e2896e24eb852d3a7b
CandelaSegura <mcsegura@udesa.edu.ar>
2024-04-21 10:48:42 -0300

    comentarios

--- miProyecto/db/index.js
+++ miProyecto/db/index.js
@@ -1,11 +1,11 @@
 let db = {
     usuario: {
-        email: 'hola',
-        usuario: 'hola',
-        contrasena: 'hola',
-        fechaDeNacimiento: 'hola',
-        documento: 'hola',
-        fotoDePerfil: 'hola',
+        email: 'juangomez@example.com',
+        usuario: 'Juan Gomez',
+        contrasena: '12345678',
+        fechaDeNacimiento: '10/05/1970',
+        documento: '1234567890',
+        fotoDePerfil: '/images/users/perfil1.jpg',
     },
     productos: [
         {   
@@ -20,11 +20,12 @@ let db = {
                     fotoDePerfil: '/images/users/perfil1.jpg'
                 },
                 {
-                    usuario: '2',
+                    usuario: '2:',
                     texto: 'El diseño de estas zapatillas es genial, ¡me encanta el estilo!',
                     fotoDePerfil: '/images/users/perfil2.jpg'
-
                 }
+                
+
             ]
         },
         {   
@@ -34,16 +35,17 @@ let db = {
             descripcion: 'Zapatillas atemporales en colores llamativos y materiales de alta costura',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '1:',
+                    texto: 'La calidad de los materiales es evidente en estas zapatillas, se ven duraderas.',
+                    fotoDePerfil: '/images/users/perfil1.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '2:',
+                    texto: '¡Me encanta el color de estas zapatillas, realmente resalta!',
+                    fotoDePerfil: '/images/users/perfil2.jpg'
 
                 }
+                
             ]
         },
         {   
@@ -53,15 +55,14 @@ let db = {
             descripcion: 'El Air Jordan 1 Low te ofrece una parte de la historia y herencia Jordan con una comodidad que dura todo el día',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '1:',
+                    texto: 'El diseño moderno de estas zapatillas de estilo futurista realmente hace que destaquen entre la multitud.',
+                    fotoDePerfil: '/images/users/perfil1.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
-
+                    usuario: '3:',
+                    texto: '¡Me encanta la atención al detalle en el acabado de estas zapatillas, se nota el cuidado en la fabricación!',
+                    fotoDePerfil: '/images/users/perfil3.jpg'
                 }
             ]
         },
@@ -72,14 +73,14 @@ let db = {
             descripcion: 'Esta iteración del AJ1 reinventa el primer modelo exclusivo de Mike con una nueva combinación de colores',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '1:',
+                    texto: 'El diseño único y colorido de estas zapatillas de estilo étnico las convierte en el centro de atención donde quiera que vaya.',
+                    fotoDePerfil: '/images/users/perfil1.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '4:',
+                    texto: 'La simplicidad elegante de estas zapatillas las hace perfectas para usar en cualquier ocasión del día a día.',
+                    fotoDePerfil: '/images/users/perfil4.jpg'
 
                 }
             ]
@@ -91,14 +92,14 @@ let db = {
             descripcion: 'Exterior de piel con superposiciones de ante y formstrip, Ojetera, lengüeta y etiqueta PUMA de material sintético',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '5:',
+                    texto: 'Estas zapatillas son mi opción número uno para el día a día; son lindas, cómodas y siempre a la moda.',
+                    fotoDePerfil: '/images/users/perfil5.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '2:',
+                    texto: 'Estas zapatillas son la combinación perfecta de estilo y funcionalidad para mis ocupados días cotidianos.',
+                    fotoDePerfil: '/images/users/perfil2.jpg'
 
                 }
             ]
@@ -110,14 +111,14 @@ let db = {
             descripcion: 'Diseño clásico de caña baja, Empeine de ante completo con forro sintético, Plantilla cómoda, Entresuela de goma, Suela exterior de goma y Cierre con cordones',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '3:',
+                    texto: 'Con estas zapatillas, cada paso es un placer. Son livianas y amortiguan perfectamente.',
+                    fotoDePerfil: '/images/users/perfil3.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '5',
+                    texto: 'Estas zapatillas son más que un calzado, son una declaración de estilo. Definitivamente elevan cualquier conjunto casual',
+                    fotoDePerfil: '/images/users/perfil5.jpg'
 
                 }
             ]
@@ -129,14 +130,14 @@ let db = {
             descripcion: 'Un icono para el dia a dia. Es difícil negar que los niños se ven realmente lindos con las zapatillas Chuck Taylor All Star',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '1:',
+                    texto: 'Estas zapatillas son como un básico en mi armario. Siempre puedo confiar en ellas para cualquier ocasión casual.',
+                    fotoDePerfil: '/images/users/perfil1.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '4:',
+                    texto: 'Estas zapatillas tienen ese toque de estilo urbano que hace que cualquier conjunto luzca genial sin esfuerzo!',
+                    fotoDePerfil: '/images/users/perfil5.jpg'
 
                 }
             ]
@@ -148,14 +149,14 @@ let db = {
             descripcion: 'Las Chuck 70 combinan los mejores detalles de las Chuck de los 70 con una confección artesanal impecable y materiales premium',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '4:',
+                    texto: 'Estas zapatillas urbanas son esenciales en cualquier guardarropa. Combinan estilo y comodidad a la perfección',
+                    fotoDePerfil: '/images/users/perfil4.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '1:',
+                    texto: 'Recomiendo estas zapatillas urbanas por su durabilidad y diseño atemporal. Son perfectas para cualquier ocasión casual!',
+                    fotoDePerfil: '/images/users/perfil1.jpg'
 
                 }
             ]
@@ -167,14 +168,14 @@ let db = {
             descripcion: 'La zapatilla para mujer 500 te proporciona el confort que buscas en tu día a día sin renunciar al estilo',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '1:',
+                    texto: 'Estas zapatillas urbanas son una excelente inversión. Su diseño minimalista las hace perfectas para cualquier conjunto',
+                    fotoDePerfil: '/images/users/perfil.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '5:',
+                    texto: 'Con estas zapatillas urbanas, no solo estarás a la moda, sino también cómodo en cualquier situación',
+                    fotoDePerfil: '/images/users/perfil5.jpg'
 
                 }
             ]
@@ -186,14 +187,14 @@ let db = {
             descripcion: 'El estilo informal se suma a la comodidad de nuestras zapatillas para mujer 515v3',
             comentarios: [
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '1',
+                    texto: 'Me encantan, súper cómodas y igual a lo que quería',
+                    fotoDePerfil: '/images/users/perfil1.jpg'
                 },
                 {
-                    usuario: 'hola',
-                    texto: 'hola',
-                    fotoDePerfil: 'hola'
+                    usuario: '5',
+                    texto: 'La verdad es que me siento supercomodo con ellas. Gran acabado y buena adaptación desde el primer momento',
+                    fotoDePerfil: '/images/users/perfil5.jpg'
 
                 }
             ]
