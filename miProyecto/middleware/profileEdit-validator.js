37ff2b666b7369ae0e0a37b657cbaadd07d8bab5
CandelaSegura <msegura@udesa.edu.ar>
2024-06-24 22:40:50 -0300

    volidator profile edit

--- miProyecto/middleware/profileEdit-validator.js
+++ miProyecto/middleware/profileEdit-validator.js
@@ -2,7 +2,35 @@ const { body } = require("express-validator");
 const db = require('../database/models');
 
 const profileEditValidation = [
-    
+    body("email")
+        .notEmpty()
+        .withMessage("Completar este campo con un email")
+        .isEmail()
+        .withMessage("Tiene que tener formato email")
+        .custom(function(value, {req}){
+            return db.User.findOne({ where: { email: value } })
+            .then(function(user) {
+                if (user) {
+                    throw new Error("Este email ya se encuentra registrado");
+                }
+            });
+    }),
+
+    body ("usuario")
+        .notEmpty()
+        .withMessage("Completar este campo con un nombre")
+        .custom(function(value, { req }) {      //Campo Adicional 
+            if (req.body.usuario[0] != req.body.usuario[0].toUpperCase()) {
+                throw new Error("El nombre de usuario debe empezar con mayúscula");
+            }
+            return true;
+        }), 
+
+    body ("contrasena")
+        .notEmpty()
+        .withMessage("Completar este campo con una contraseña")
+        .isLength({min:4})
+        .withMessage("Debe tener al menos 4 caracteres"),
 ];
 
 module.exports = profileEditValidation;
